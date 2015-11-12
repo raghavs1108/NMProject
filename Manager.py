65209ffc460f372298b45b8290f302b0ff18e2f9
raghavs1108 <raghavs1108@gmail.com>
2015-11-12 14:54:04 +0530

    initial commit

---
+++ Manager.py
@@ -0,0 +1,60 @@
+from pysnmp.carrier.asynsock.dispatch import AsynsockDispatcher
+from pysnmp.carrier.asynsock.dgram import udp
+from pyasn1.codec.ber import decoder
+from pysnmp.proto import api
+
+def cbFun(transportDispatcher, transportDomain, transportAddress, wholeMsg):
+    while wholeMsg:
+        msgVer = int(api.decodeMessageVersion(wholeMsg))
+        if api.protoModules.has_key(msgVer):
+            pMod = api.protoModules[msgVer]
+        else:
+            print 'Unsupported SNMP version %s' % msgVer
+            return
+        reqMsg, wholeMsg = decoder.decode(wholeMsg, asn1Spec=pMod.Message(),)
+        
+        print 'Notification message from %s:%s: ' % ( transportDomain, transportAddress )
+        reqPDU = pMod.apiMessage.getPDU(reqMsg)
+        
+        if reqPDU.isSameTypeWith(pMod.TrapPDU()):
+            if msgVer == api.protoVersion1:
+                print 'Enterprise: %s' % (
+                    pMod.apiTrapPDU.getEnterprise(reqPDU).prettyPrint()
+                    )
+                print 'Agent Address: %s' % (
+                    pMod.apiTrapPDU.getAgentAddr(reqPDU).prettyPrint()
+                    )
+                print 'Generic Trap: %s' % (
+                    pMod.apiTrapPDU.getGenericTrap(reqPDU).prettyPrint()
+                    )
+                print 'Specific Trap: %s' % (
+                    pMod.apiTrapPDU.getSpecificTrap(reqPDU).prettyPrint()
+                    )
+                print 'Uptime: %s' % (
+                    pMod.apiTrapPDU.getTimeStamp(reqPDU).prettyPrint()
+                    )
+                varBinds = pMod.apiTrapPDU.getVarBindList(reqPDU)
+            else:
+                varBinds = pMod.apiPDU.getVarBindList(reqPDU)
+            print 'Var-binds:'
+            for oid, val in varBinds:
+                print '%s = %s' % (oid.prettyPrint(), val.prettyPrint())
+                print wholeMsg
+    return wholeMsg
+
+try:
+    transportDispatcher = AsynsockDispatcher()
+    transportDispatcher.registerTransport(
+        udp.domainName, udp.UdpSocketTransport().openServerMode(('192.168.0.104', 8090))
+        )
+    transportDispatcher.registerRecvCbFun(cbFun)
+    transportDispatcher.jobStarted(1) # this job would never finish
+    transportDispatcher.runDispatcher()
+except Exception, e:
+    pass
+except KeyboardInterrupt, e:
+    pass
+else:
+    pass
+finally:
+    pass
